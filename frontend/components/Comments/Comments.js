40537beb19bd3de47456d7f1118bf3bdd9dcc52d
MrHyde801 <ianhyde801@gmail.com>
2024-10-29 15:41:17 -0600

    finished module project

--- frontend/components/Comments/Comments.js
+++ frontend/components/Comments/Comments.js
@@ -7,7 +7,10 @@ const Comments = props => {
 
   return (
     <div className='comments-wrapper'>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {/* map through the comments prop and render a Comment for every piece of data */
+      comments.map(comment => 
+      <Comment key={comment.id} comment={comment} />
+      )}
     </div>
   );
 };
